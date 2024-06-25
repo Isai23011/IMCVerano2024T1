ed9c29107791d03651dd2d84c34b27420903dcc5
Isai Montaño Chavez <230110077@itsoeh.edu.mx>
2024-06-25 11:58:50 -0600

    Correccion de etiquetas

--- caracas_2.py
+++ caracas_2.py
@@ -4,7 +4,7 @@ def plot_hist(clases, freq_absoluta, mrks, labelx, labely, titulo):
     plt.figure(figsize=(12, 6))  # Set the figure size
 
     plt.bar(mrks, freq_absoluta,
-           width=50, edgecolor="k",
+           width=0.3, edgecolor="k",
            color=["#14BF48", "#33FFBE", "#333CFF", "#FF3349", "#F6FF33", "#33FFBE"])
     
     plt.xticks(mrks,  fontsize=12)
